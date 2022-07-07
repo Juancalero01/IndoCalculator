2faf55804c9c2f0c8bd8c445e30a77b908da0725
Juan Calero <marcelocalero5@gmail.com>
2022-07-07 17:39:35 -0300

    new commit

--- js/main.js
+++ js/main.js
@@ -2,26 +2,46 @@ let inputValueOne = document.getElementById("input-one");
 let inputValueTwo = document.getElementById("input-two");
 let inputValueThree = document.getElementById("input-three");
 let selectValueOne = document.getElementById("select-one");
-let minNumber, timerId;
+let span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById("modal");
+let price = document.getElementById("priceop");
+let minNumber;
 
-function calcular() {
+function getDetail() {
     if ((inputValueOne.value && inputValueTwo.value) != 0 && (inputValueThree.value != 0 || inputValueThree.value == 0)) {
         if (inputValueThree.value == 0) {
             minNumber = Math.min(inputValueOne.value, inputValueTwo.value);
             if (selectValueOne.options[selectValueOne.selectedIndex].value != 0) {
-                document.getElementById("valueInput").innerHTML = sumar(minNumber);
+                modal.style.display = "block";
+                price.style.display = "none";
+                document.getElementById("priceOne").innerHTML = inputValueOne.value;
+                document.getElementById("priceTwo").innerHTML = inputValueTwo.value;
+                document.getElementById("desc").innerHTML = selectValueOne.options[selectValueOne.selectedIndex].value + "%";
+                document.getElementById("pricemin").innerHTML = minNumber;
+                document.getElementById("pricemindesc").innerHTML = Math.floor(minNumber * selectValueOne.options[selectValueOne.selectedIndex].value) / 100;
+                document.getElementById("subtotal").innerHTML = getSubtotal();
+                document.getElementById("total").innerHTML = getTotal(minNumber);
             }
         }
         else {
             minNumber = Math.min(inputValueOne.value, inputValueTwo.value, inputValueThree.value);
             if (selectValueOne.options[selectValueOne.selectedIndex].value != 0) {
-                document.getElementById("valueInput").innerHTML = sumar(minNumber);
+                modal.style.display = "block";
+                price.style.display = "";
+                document.getElementById("priceOne").innerHTML = inputValueOne.value;
+                document.getElementById("priceTwo").innerHTML = inputValueTwo.value;
+                document.getElementById("priceThree").innerHTML = inputValueThree.value;
+                document.getElementById("desc").innerHTML = selectValueOne.options[selectValueOne.selectedIndex].value + "%";
+                document.getElementById("pricemin").innerHTML = minNumber;
+                document.getElementById("pricemindesc").innerHTML = Math.floor(minNumber * selectValueOne.options[selectValueOne.selectedIndex].value) / 100;
+                document.getElementById("subtotal").innerHTML = getSubtotal();
+                document.getElementById("total").innerHTML = getTotal(minNumber);
             }
         }
     }
 }
 
-function sumar(minNumber) {
+function getTotal(minNumber) {
     if (inputValueThree.value == 0) {
         if (minNumber == inputValueOne.value) {
             return Number(inputValueTwo.value) + Math.floor(minNumber * selectValueOne.options[selectValueOne.selectedIndex].value) / 100;
@@ -43,10 +63,25 @@ function sumar(minNumber) {
     }
 }
 
-function limpiarCampos() {
+function getSubtotal() {
+    return Number(inputValueOne.value) + Number(inputValueTwo.value) + Number(inputValueThree.value);
+}
+
+function clearFields() {
     inputValueOne.value = "";
     inputValueTwo.value = "";
     inputValueThree.value = "";
     selectValueOne.selectedIndex = 0;
     document.getElementById("valueInput").innerHTML = "";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
